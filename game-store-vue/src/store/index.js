9ecc82a32d8d741ddfd6bb6e5d2c5e5d90169f03
Cabbage-xy <cxy4915@gmail.com>
2022-06-05 15:26:13 +0800

    增加注册对话框显示状态相关

--- game-store-vue/src/store/index.js
+++ game-store-vue/src/store/index.js
@@ -3,6 +3,7 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     showLoginDialog: false,
+    showRegisterDialog: false,
   },
   getters: {
   },
@@ -12,6 +13,12 @@ export default createStore({
     },
     closeLoginDialog(state) {
       state.showLoginDialog = false;
+    },
+    showRegisterDialog(state) {
+      state.showRegisterDialog = true;
+    },
+    closeRegisterDialog(state) {
+      state.showRegisterDialog = false;
     }
   },
   actions: {
