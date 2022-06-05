3de2a917481dc4643c9a4d60085aa6f2a93d6c26
Cabbage-xy <cxy4915@gmail.com>
2022-06-05 15:26:27 +0800

    注册对话框

---
+++ game-store-vue/src/components/RegisterDialog.vue
@@ -0,0 +1,77 @@
+<template>
+  <el-dialog v-model="dialogFormVisible" :append-to-body="false" title="注册" @closed="closeDialog()">
+    <el-form ref="login" :rules="rules" :model="form">
+      <el-form-item prop="name" label="用户名" :label-width="formLabelWidth">
+        <el-input v-model="form.name" placeholder="请输入用户名" autocomplete="off" />
+      </el-form-item>
+      <el-form-item prop="password" label="密码" :label-width="formLabelWidth">
+          <el-input type="password" v-model="form.password" placeholder="请输入密码" autocomplete="off" />
+      </el-form-item>
+      <el-form-item prop="checkPassword" label="重复密码" :label-width="formLabelWidth" @keyup.enter="handleRegister()">
+          <el-input type="password" v-model="form.checkPassword" placeholder="请再次输入密码" autocomplete="off" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="closeDialog()">取消</el-button>
+        <el-button type="primary" @click="handleRegister()">注册</el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+<script>
+import { ref, reactive, computed } from 'vue';
+import { ElMessage } from "element-plus";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+export default ({
+    name: "RegisterDialog",
+    setup() {
+        const router = useRouter();
+        const formLabelWidth = '140px';
+        const rules = {
+            name: [
+                { required: true, message: "请输入用户名", trigger: "blur" },
+            ],
+            password: [
+                { required: true, message: "请输入密码", trigger: "blur" },
+            ],
+            checkPassword: [
+                { required: true, message: "请输入密码", trigger: "blur" },
+            ],
+        };
+        const form = reactive({
+            name: "",
+            password: ""
+        });
+        const login = ref(null);
+        const handleRegister = () => {
+            login.value.validate((valid) => {
+                if (valid) {
+                    ElMessage.success("登录成功");
+                    localStorage.setItem("ms_username", form.username);
+                    router.push("/");
+                } else {
+                    ElMessage.error("登录成功");
+                    return false;
+                }
+            });
+        };
+        const store = useStore();
+        const dialogFormVisible = computed(() => store.state.showRegisterDialog);
+        const closeDialog = () => {
+            store.commit("closeRegisterDialog");
+        }
+        return {
+            dialogFormVisible,
+            formLabelWidth,
+            rules,
+            form,
+            login,
+            handleRegister,
+            closeDialog
+        }
+        
+    },
+})
+</script>
