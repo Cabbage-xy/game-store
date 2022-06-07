14079b57995a68ea6f780bbb0bc6c89ee415b541
Cabbage-xy <cxy4915@gmail.com>
2022-06-08 01:26:08 +0800

    端口配置

--- game-store-vue/vue.config.js
+++ game-store-vue/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 80,
+    https: false,
+    oprn: true,
+    proxy: {},
+
+  }
 })
