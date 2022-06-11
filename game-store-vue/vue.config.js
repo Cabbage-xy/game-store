adf776202a9793a1bdad0bc825c79a1a7048cef5
Cabbage-xy <cxy4915@gmail.com>
2022-06-12 02:52:29 +0800

    杂项

--- game-store-vue/vue.config.js
+++ game-store-vue/vue.config.js
@@ -1,15 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  lintOnSave: false,
   devServer: {
     host: '0.0.0.0',
-    port: 80,
+    port: 8081,
     https: false,
-    oprn: true,
+    open: false,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
-        changeOrgin: true,
+        changeOrgin: true, //是否同源
         pathRewrite: {
           '/api': ''
         }
